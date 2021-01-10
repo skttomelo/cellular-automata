924c0f52ce460e3882243dd8610eab6a2909eb49
Trevor Crow <14996662+skttomelo@users.noreply.github.com>
2021-01-10 08:42:44 -0500

    Added some water cellular automata

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ screen = pygame.display.set_mode((800,600))
 
 pygame.display.set_caption("Save me bruv")
 
-SCALE = 50
+SCALE = 25
 
 clicked_position = [-1,-1]
 
@@ -34,37 +34,95 @@ def events():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if pygame.mouse.get_pressed()[0]:
+    if pygame.mouse.get_pressed()[0]:
+        try:
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pos_scaled = (mouse_pos[0]//SCALE, mouse_pos[1]//SCALE)
+            if pixels[mouse_pos_scaled[1]][mouse_pos_scaled[0]] == 0:
+                pixels[mouse_pos_scaled[1]][mouse_pos_scaled[0]] = 1
+        except AttributeError:
+            pass
+    if pygame.mouse.get_pressed()[2]:
             try:
                 mouse_pos = pygame.mouse.get_pos()
                 mouse_pos_scaled = (mouse_pos[0]//SCALE, mouse_pos[1]//SCALE)
                 if pixels[mouse_pos_scaled[1]][mouse_pos_scaled[0]] == 0:
-                    pixels[mouse_pos_scaled[1]][mouse_pos_scaled[0]] = 1
+                    pixels[mouse_pos_scaled[1]][mouse_pos_scaled[0]] = 2
             except AttributeError:
                 pass
 
 def update():
     for y in range(600//SCALE):
         for x in range(800//SCALE):
+            # we go through our array backwards that way we can see the effects live rather than everything being calculated before you see an update
+
             # nothing
             if pixels[(600//SCALE) - y - 1][(800//SCALE) - x - 1] == 0:
                 continue
+            try:
+                up = pixels[(600//SCALE) - y - 2][(800//SCALE) - x - 1]
+            except:
+                up = -1
+            try:
+                down = pixels[(600//SCALE) - y][(800//SCALE) - x - 1]
+            except:
+                down = -1
+            try:
+                down_left = pixels[(600//SCALE) - y][(800//SCALE) - x - 2]
+            except:
+                down_left = -1
+            try:
+                down_right = pixels[(600//SCALE) - y][(800//SCALE) - x]
+            except:
+                down_right = -1
+            try:
+                left = pixels[(600//SCALE) - y - 1][(800//SCALE) - x - 2]
+            except:
+                left = -1
+            try:
+                right = pixels[(600//SCALE) - y - 1][(800//SCALE) - x]
+            except:
+                right = -1
             # sand
-            elif pixels[(600//SCALE) - y - 1][(800//SCALE) - x - 1] == 1:
-                # we need to check if there is anything below it or beside it, and move it
-                try:
-                    if pixels[(600//SCALE) - y][(800//SCALE) - x - 1] == 0:
-                        pixels[(600//SCALE) - y - 1][(800//SCALE) - x - 1] = 0
-                        pixels[(600//SCALE) - y][(800//SCALE) - x - 1] = 1
-                    # there is something below it so we check the bottom left if there is nothing
-                    elif pixels[(600//SCALE) - y][(800//SCALE) - x - 2] == 0:
-                        pixels[(600//SCALE) - y - 1][(800//SCALE) - x - 1] = 0
-                        pixels[(600//SCALE) - y][(800//SCALE) - x - 2] = 1
-                    elif pixels[(600//SCALE) - y][(800//SCALE) - x] == 0:
-                        pixels[(600//SCALE) - y - 1][(800//SCALE) - x - 1] = 0
-                        pixels[(600//SCALE) - y][(800//SCALE) - x] = 1
-                except:
-                    pass
+            if pixels[(600//SCALE) - y - 1][(800//SCALE) - x - 1] == 1:
+                '''
+                sand should flow like this (whether there is nothing or liquids):
+                          #
+                        / | \
+                       <  V  >
+                '''
+                if down == 0:
+                    pixels[(600//SCALE) - y - 1][(800//SCALE) - x - 1] = 0
+                    pixels[(600//SCALE) - y][(800//SCALE) - x - 1] = 1
+                elif down_left == 0:
+                    pixels[(600//SCALE) - y - 1][(800//SCALE) - x - 1] = 0
+                    pixels[(600//SCALE) - y][(800//SCALE) - x - 2] = 1
+                elif down_right == 0:
+                    pixels[(600//SCALE) - y - 1][(800//SCALE) - x - 1] = 0
+                    pixels[(600//SCALE) - y][(800//SCALE) - x] = 1
+            # water
+            elif pixels[(600//SCALE) - y - 1][(800//SCALE) - x - 1] == 2:
+                '''
+                water should move like this (when there is nothing):
+                      < - # - >
+                        / | \
+                       <  V  >
+                '''
+                if down == 0:
+                    pixels[(600//SCALE) - y - 1][(800//SCALE) - x - 1] = 0
+                    pixels[(600//SCALE) - y][(800//SCALE) - x - 1] = 2
+                elif down_left == 0:
+                    pixels[(600//SCALE) - y - 1][(800//SCALE) - x - 1] = 0
+                    pixels[(600//SCALE) - y][(800//SCALE) - x - 2] = 2
+                elif down_right == 0:
+                    pixels[(600//SCALE) - y - 1][(800//SCALE) - x - 1] = 0
+                    pixels[(600//SCALE) - y][(800//SCALE) - x] = 2
+                elif left == 0:
+                    pixels[(600//SCALE) - y - 1][(800//SCALE) - x - 1] = 0
+                    pixels[(600//SCALE) - y - 1][(800//SCALE) - x - 2] = 2
+                elif right == 0:
+                    pixels[(600//SCALE) - y - 1][(800//SCALE) - x - 1] = 0
+                    pixels[(600//SCALE) - y - 1][(800//SCALE) - x] = 2
 
 def draw():
     for y in range(600//SCALE):
