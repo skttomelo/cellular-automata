fa06d70f05051e51b83d232b74092c12bbd2331d
Trevor Crow <14996662+skttomelo@users.noreply.github.com>
2021-01-10 04:33:17 -0500

    swapped to GGEZ for now; boiler-plate done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,104 @@
-fn main() {
-    // just making this
-    println!("Hello, world!");
+use ggez;
+use ggez::conf::{FullscreenType, WindowMode};
+use ggez::event::{self, EventHandler, KeyCode, KeyMods};
+use ggez::graphics;
+use ggez::graphics::{DrawParam, FilterMode, Font, Image, Rect};
+use ggez::input::mouse::MouseButton;
+use ggez::nalgebra::Point2;
+use ggez::{Context, GameResult};
+
+use std::env;
+
+const SCREEN_WIDTH: f32 = 800.0;
+const SCREEN_HEIGHT: f32 = 600.0;
+
+struct MainState {
+    
+}
+
+impl MainState {
+    fn new() -> GameResult<MainState> {
+        
+
+        let s = MainState {
+            
+        };
+        Ok(s)
+    }
+}
+
+impl EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
+
+        // draw background
+        // don't need
+
+        // draw everything else
+
+        graphics::present(ctx)?;
+        Ok(())
+    }
+
+    fn key_down_event(
+        &mut self,
+        _ctx: &mut Context,
+        keycode: KeyCode,
+        _keymods: KeyMods,
+        _repeat: bool,
+    ) {
+        unimplemented!();
+    }
+
+    fn key_up_event(&mut self, _ctx: &mut Context, keycode: KeyCode, keymods: KeyMods) {
+        unimplemented!();
+    }
+
+    fn mouse_motion_event(&mut self, _ctx: &mut Context, x: f32, y: f32, _dx: f32, _dy: f32) {
+        unimplemented!();
+    }
+
+    fn mouse_button_up_event(&mut self, _ctx: &mut Context, button: MouseButton, x: f32, y: f32) {
+        unimplemented!();
+    }
+
+    fn mouse_button_down_event(&mut self, _ctx: &mut Context, button: MouseButton, x: f32, y: f32) {
+        unimplemented!();
+    }
+}
+
+pub fn main() -> GameResult {
+
+    // window
+    let window = WindowMode {
+        width: SCREEN_WIDTH,
+        height: SCREEN_HEIGHT,
+        maximized: false,
+        fullscreen_type: FullscreenType::Windowed,
+        borderless: false,
+        min_width: 0.0,
+        max_width: 0.0,
+        min_height: 0.0,
+        max_height: 0.0,
+        resizable: false,
+    };
+
+    let mut cb = ggez::ContextBuilder::new("Vagabond Client", "Trevor Crow");
+
+    // get and add resource path
+    if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+        let mut path = PathBuf::from(manifest_dir);
+        path.push("resources");
+        println!("Adding path {:?}", path);
+        cb = cb.add_resource_path(path);
+    }
+
+    // build and split context builder with window configuration
+    let (ctx, event_loop) = &mut cb.window_mode(window).build()?;
+    let state = &mut MainState::new()?;
+    event::run(ctx, event_loop, state)
 }
