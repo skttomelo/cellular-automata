36e936414ad0bdfd26cf1b6cb5a84ac869ee8847
Trevor Crow <14996662+skttomelo@users.noreply.github.com>
2021-01-11 20:26:00 -0500

    water sim added now

--- main.py
+++ main.py
@@ -100,7 +100,67 @@ system_manager.add_system(SandSystem())
         
 
 # water system (TODO)
+class WaterSystem(ecs.models.System):
+    def update(self, dt):
+        # get all entities in database that have PixelType
+        entity_list = self.entity_manager.pairs_for_type(PixelType)
+        
+        # loop through each entity checking to see if it has the color yellow
+        # after that we then look for entities within 1 pixel of it respectfully
+        # aka down-left | down | down-right
+        for entity in entity_list:
+            if entity[1].color != Color.blue:
+                continue
+            entity_pos = self.entity_manager.component_for_entity(entity[0], Position)
 
+            # sanity checks
+            if entity_pos.y + 1 >= 600//SCALE: # unknown if I will ever implement gravity that is greater than 1
+                continue
+            if entity_pos.x + 1 == 800//SCALE:
+                continue
+            if entity_pos.x + 1 < 0:
+                continue
+
+            # now we need to figure out if the entity will move down, down to the side, or not at all
+            directions = [False, False, False, False, False] # indexes - 0 = down, 1 = down-left, 2 = down-right (if true then that direction is blocked)
+            '''
+                water should move like this (when there is nothing):
+                    < - # - >
+                      / | \
+                     <  V  >
+            '''
+            new_list = self.entity_manager.pairs_for_type(Position)
+            for other_entity in new_list:
+                pos = self.entity_manager.component_for_entity(other_entity[0], Position)
+                if other_entity[0] == entity[0]:
+                    continue
+
+                if entity_pos.x == pos.x and entity_pos.y+1 == pos.y:
+                    directions[0] = True
+                elif entity_pos.x-1 == pos.x and entity_pos.y+1 == pos.y:
+                    directions[1] = True
+                elif entity_pos.x+1 == pos.x and entity_pos.y+1 == pos.y:
+                    directions[2] = True
+                elif entity_pos.x-1 == pos.x and entity_pos.y == pos.y:
+                    directions[3] = True
+                elif entity_pos.x+1 == pos.x and entity_pos.y == pos.y:
+                    directions[4] = True
+            
+            if directions[0] == False:
+                entity_pos.y += 1
+            elif directions[1] == False:
+                entity_pos.y += 1
+                entity_pos.x -= 1
+            elif directions[2] == False:
+                entity_pos.y += 1
+                entity_pos.x += 1
+            elif directions[3] == False:
+                entity_pos.x -= 1
+            elif directions[4] == False:
+                entity_pos.x += 1
+
+# add WaterSystem to system manager
+system_manager.add_system(WaterSystem())
 
 def events():
     for event in pygame.event.get():
@@ -125,14 +185,22 @@ def events():
         except AttributeError:
             pass
     # right mouse button pressed
-    # if pygame.mouse.get_pressed()[2]:
-    #         try:
-    #             mouse_pos = pygame.mouse.get_pos()
-    #             mouse_pos_scaled = (mouse_pos[0]//SCALE, mouse_pos[1]//SCALE)
-    #             if pixels[mouse_pos_scaled[1]][mouse_pos_scaled[0]] == 0:
-    #                 pixels[mouse_pos_scaled[1]][mouse_pos_scaled[0]] = 2
-    #         except AttributeError:
-    #             pass
+    if pygame.mouse.get_pressed()[2]:
+        try:
+            new_entity = entity_manager.create_entity()
+            mouse_pos = pygame.mouse.get_pos()
+            pos = Position(mouse_pos[0]//SCALE, mouse_pos[1]//SCALE)
+            empty = True
+            for entity in entity_manager.pairs_for_type(Position):
+                if entity[1].x == pos.x and entity[1].y == pos.y:
+                    empty = False
+                    break
+            
+            if empty == True:
+                entity_manager.add_component(new_entity, pos)
+                entity_manager.add_component(new_entity, PixelType(Color.blue))
+        except AttributeError:
+            pass
 
 def update():
     system_manager.update(datetime.datetime.now().timestamp())
